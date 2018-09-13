d96c31fecdcff6d0b261e518814cfa82911ce411
Gina Fedder <ginafedder@yahoo.com>
2018-09-13 15:59:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -33,6 +33,10 @@ def last_element_with_array_methods(south_east_asia)
   last_country.last
 end
 
-def length_of_array()
+def length_of_array(programming_languages)
+  length = [programming_languages]
+  length.length
+end
+
   
   
