ac8421ae1859c207e420dfcb8fc21fbd8cb1dd69
Gina Fedder <ginafedder@yahoo.com>
2018-09-13 16:01:59 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,28 +8,23 @@ def array_with_two_elements
   return my_two_array
 end
 
-def first_element(taylor_swift)
-  my_first_element = [taylor_swift]
+def first_element(my_first_element)
   my_first_element[0]
 end
 
-def third_element(taylor_swift)
-  my_third_element = [taylor_swift]
+def third_element(my_third_element)
   my_third_element[2]
 end
 
-def last_element(taylor_swift)
-  my_last_element = [taylor_swift]
+def last_element(my_last_element)
   my_last_element[-1]
 end
 
-def first_element_with_array_methods(south_east_asia)
-  first_country = [south_east_asia]
+def first_element_with_array_methods(first_country)
   first_country.first
 end
 
-def last_element_with_array_methods(south_east_asia)
-  last_country = [south_east_asia]
+def last_element_with_array_methods(last_country)
   last_country.last
 end
 
