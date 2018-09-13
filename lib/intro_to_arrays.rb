28db002768600813175e98880ee469d32752406b
Gina Fedder <ginafedder@yahoo.com>
2018-09-13 15:40:09 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,1 +1,13 @@
-my_new_array = []
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+
+def array_with_two_elements()
+  my_two_array = ["one", "two"]
+  return my_two_array
+end
+
+def first_element(element)
+  my_first_element = [element]
+  return my_first_element[0]
